261ee5f186c072fdc602fc33963b35e877e1b269
teddy-b <teddyboneva@gmail.com>
2016-04-11 00:35:06 +0300

    index.js fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,8 @@
 window.onload = function() {
-    // $.ajax({
-    //     url:  "/userInfo",
-    //     success: function(data) {
+    $.ajax({
+        url:  "/userInfo",
+        success: function(data) {
             var game = new Game();
-
-            data = {
-                "id":3,
-                "name":"administrator",
-                "email":"admin@admin.com",
-                "active":0,
-                "coins":10,
-                "mario":3,
-                "mushroom":0,
-                "shooting":1,
-                "double_jump":1,
-                "low_gravity":0,
-                "games_played":0,
-                "score":20,
-                "level_reached":0,
-                "highest_score":0,
-                "avatar":"profile\/XkqlJl9t4qDIjrQ5g2n4Yf3CNTOvHx-chuck_norris.jpg",
-                "social":0,
-                "created_at":"2016-04-04 23:34:29",
-                "updated_at":"2016-04-09 19:20:23",
-                "user":1
-            };
 
             game.userId = data.id;
             game.coins = data.coins;
@@ -38,6 +16,6 @@ window.onload = function() {
             game.levelReached = data.level_reached;
 
             game.start();
-        // }
-    // });
+        }
+    });
 };
