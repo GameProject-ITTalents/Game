d94485bc27ddadc0bfab30aab5e95078e3940b27
Teodora Boneva <teddyboneva@gmail.com>
2017-11-23 18:26:01 +0200

    Fix state background

--- public/js/states/menu.js
+++ public/js/states/menu.js
@@ -6,14 +6,14 @@ var MenuState = (function() {
 	}
 
 	MenuState.prototype = Object.create(State.prototype);
-	MenuState.prototype.constructor = MenuState();
+	MenuState.prototype.constructor = MenuState;
 
 	MenuState.prototype.create = function() {
 		State.prototype.create.call(this);
 
-		this.background = this.game.add.sprite(0, 0, this.background);
-		this.background.height = this.game.height;
-		this.background.width = this.game.width;
+		this.game.background = this.game.add.sprite(0, 0, this.background);
+		this.game.background.height = this.game.height;
+		this.game.background.width = this.game.width;
 
 		this.game.add.button(10, 10, 'home', goToHome, this, 2, 1, 0).fixedToCamera = true;
 		this.game.add.button(30, 10, 'cart', goToShop, this, 2, 1, 0).fixedToCamera = true;
